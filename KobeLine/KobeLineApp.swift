7f3c6959b99f857ef4d3f630a33453b94836ae07
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2023-06-17 14:05:53 +0900

    Initial Commit

---
+++ KobeLine/KobeLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  KobeLineApp.swift
+//  KobeLine
+//
+//  Created by 田中大地 on 2023/06/17.
+//
+
+import SwiftUI
+
+@main
+struct KobeLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
